8b093cad8942417819a6c7d3eac3c9d405876726
Joe Miller <joewmiller19@gmail.com>
2021-01-11 17:56:11 -0500

    Finish SuperComputerPlayer so it never loses
    Implemented SuperComputerPlayer from the provided skeleton. The
    super computer uses TicTacToeNode to find the tree of all possible
    moves. It will always choose a winning move unles none are available,
    in which case it will choose a move that leads to a draw.

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,21 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    move_tree = TicTacToeNode.new(game.board, mark).children
+
+    # Pick a winning move if it exists
+    move_tree.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    # If there are no winning moves, pick any non-losing move (force draw)
+    move_tree.each do |child|
+      return child.prev_move_pos unless child.losing_node?(mark)
+    end
+
+    raise "Something went wrong! Could not find a move that causes a win or draw."
   end
 end
 
